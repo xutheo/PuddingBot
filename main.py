fa84fc25138a1fc8f5f9ae941a818750b8ebfe38
xutheo <zalteoxutheo@gmail.com>
2023-09-12 23:20:49 -0700

    Added support for retrieval from google sheets

--- main.py
+++ main.py
@@ -7,20 +7,29 @@ from discord.commands import Option
 import sheets_helper
 import json
 import unicodedata
+#from table2ascii import table2ascii as t2a, PresetStyle
+#import pandas as pd
+#from tabulate import tabulate
 
 tabulate.PRESERVE_WHITESPACE = True
 
 # Cache the translation mapping
 translation_mapping = sheets_helper.get_translation_mapping()
+tls = {1: sheets_helper.Timelines(1),
+       2: sheets_helper.Timelines(2),
+       3: sheets_helper.Timelines(3),
+       4: sheets_helper.Timelines(4),
+       5: sheets_helper.Timelines(5)}
 
-# Define our bot
+       # Define our bot
 guild_id = [1002644143589302352, 1025780100291112960]  # Server ids
 channel_id = [1067620591038889995, 1141149506021367849]  # Channel ids
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("."))
 
 
 async def is_allowed_channel(ctx):
-    return ctx.channel.id in channel_id
+    #return ctx.channel.id in channel_id
+    return True
 
 
 async def convert_and_translate_timeline(tl, translate=True):
@@ -45,6 +54,12 @@ async def on_ready():
     print("-------------------------")
 
 
+@bot.slash_command(description="Load TLs")
+async def load_tls(ctx, boss):
+    global tls
+    tls[int(boss)] = sheets_helper.Timelines(boss)
+
+
 # =============== Translate TL =============
 @bot.slash_command(guild_ids=guild_id, description="Translate TL")
 async def translate_tl(
@@ -61,8 +76,10 @@ async def translate_tl(
     await ctx.respond(embed=embed, ephemeral=(not show))
 
 
+'''
 # =============== Transcribe TL =============
 @bot.slash_command(guild_ids=guild_id, description="Transcribe TL")
+@commands.has_role(1025780684574433390)
 async def transcribe_tl(
         ctx,
         boss: Option(int, "1 - 5", min_value=1, max_value=5),
@@ -116,28 +133,121 @@ async def transcribe_tl(
                         color=0xfffeff)
     await ctx.respond(embed=embed, ephemeral=True)
     await ctx.respond("Would you like to save this tl?", view=view, ephemeral=True)
+'''
 
 
 # =============== Lists one timeline with specified id =============
-@bot.slash_command(guild_ids=guild_id, description="Gets target TL")
-async def list_tl(
+@bot.slash_command(description="Gets target TL")
+async def get_tl(
         ctx,
         id,
         show: Option(bool, "Show this timeline to everyone", required=False, default=False)):
-    boss = id[1]
-    row = int(id[2:])
+    boss = int(id[1])
+    mobile = ctx.author.is_on_mobile()
+    if mobile:
+        await ctx.respond("Please use this command on PC for the best viewing experience.")
 
-    tl_worksheet = sheets_helper.get_timelines_worksheet(boss)
-    timeline = tl_worksheet.get_value('B' + str(row))
-
-    if not timeline or id[2:] == '01':
+    if id not in tls[boss].timelines:
         await ctx.respond("A timeline with that ID does not exist!", ephemeral=True)
         return
 
-    embed = discord.Embed(title=id,
-                        description=timeline,
-                        color=0xfffeff)
+    timeline = tls[boss].timelines[id]
+    embed = discord.Embed(
+        type="rich",
+        title=id,
+        description=f'Author: {timeline.author}\nTranscriber: {timeline.transcriber}\nEV: {timeline.ev}\nST_DEV: {timeline.st_dev}\nStyle: {timeline.style}\nStatus: {timeline.status}',
+        color=0xffffff,
+        url='https://docs.google.com/spreadsheets/d/1Zytb-0_ln6WARlCgn3opy-lfnuUr76x83iBNpUnu5wE/edit#gid=' + str(timeline.sheet_id) + '&range=' + chr(timeline.starting_cell_tuple[1] + 64) + str(timeline.starting_cell_tuple[0]))
+    embed.set_thumbnail(url=timeline.thumbnail_url)
+
+    embed_limit = 1012
+    current_char = 0
+    unit_first_total_line_limit = 42  # Dynamic calculation needed when unit column is present
+    total_line_limit = 40  # Dynamic calculation needed when unit column is present
+    max_time_length = max([len(x[0]) for x in timeline.tl_actions])
+    max_unit_length = max([len(x[1]) for x in timeline.tl_actions])
+    line_limit = total_line_limit - max_time_length - 4
+    if timeline.unit_column:
+        line_limit = unit_first_total_line_limit - max([len(x[1]) for x in timeline.tl_actions]) - max_time_length + 1
+    print("line_limit: " + str(line_limit))
+
+    flex_string = ''  # Either set status column or unit column
+    time_string = ''
+    action_string = ''
+
+    def get_number_of_extra_lines(text):
+        print(text)
+        if len(text) <= line_limit:
+            return 0
+        line_split = text.split("\n")
+        extra_lines = len(line_split) - 1
+        print(line_split)
+        for line in line_split:
+            char_count = 0
+            action_split = line.split(" ")
+            for act in action_split:
+                if len(act) + char_count > line_limit:
+                    extra_lines += 1
+                    char_count = 0
+                char_count += len(act) + 1
+        return extra_lines
+
+    for i in range(0, len(timeline.tl_actions)):
+        action = timeline.tl_actions[i]
+        action_description = action[1] if not timeline.unit_column else action[2]
+        flex = action[2] if not timeline.unit_column else action[1]
+        number_of_extra_lines = get_number_of_extra_lines(action_description)
+        if current_char + len(action_description) + line_limit - 4 >= embed_limit:
+            time_string = '\n\n' + time_string # For some reason the first two lines of this string gets cut off in the below embed
+            flex_string = '\n\n' + flex_string # For some reason the first two lines of this string gets cut off in the below embed
+            action_string = '\n\n' + action_string # For some reason the first two lines of this string gets cut off in the below embed
+            embed.add_field(
+                name="Time",
+                value=f"```{time_string}```",
+                inline=True)
+            embed.add_field(
+                name="Unit" if timeline.unit_column else "Action Description",
+                value=f"```{action_string if not timeline.unit_column else flex_string}```",
+                inline=True)
+            embed.add_field(
+                name="Action Description" if timeline.unit_column else "Set Status",
+                value=f"```{flex_string if not timeline.unit_column else action_string}```",
+                inline=True)
+            print("actual length: " + str(len(f"```{flex_string if not timeline.unit_column else action_string}```")))
+            flex_string = ''
+            time_string = ''
+            action_string = ''
+            current_char = 0
+
+        time_string += action[0] + (number_of_extra_lines + 1) * "\n"
+        flex_string += flex + (number_of_extra_lines + 1) * "\n"
+        action_string += action_description + '\n'
+        current_char += len(action_description) + 1
+        if i != len(timeline.tl_actions) - 1 and len(timeline.tl_actions[i+1][0]) != 0:
+            action_string += '-' * (line_limit - 5) + "\n"
+            time_string += '-' * max_time_length + "\n"
+            flex_string += '-' * min(14, max_unit_length) + "\n" if timeline.unit_column else '-' * 5 + "\n"
+            current_char += line_limit - 4
+        print("current_char: " + str(current_char) + " action_string_length: " + str(len(action_string)))
+
+    time_string = '\n\n' + time_string  # For some reason the first two lines of this string gets cut off in the below embed
+    flex_string = '\n\n' + flex_string  # For some reason the first two lines of this string gets cut off in the below embed
+    action_string = '\n\n' + action_string  # For some reason the first two lines of this string gets cut off in the below embed
+    embed.add_field(
+        name="Time",
+        value=f"```{time_string}```",
+        inline=True)
+    embed.add_field(
+        name="Unit" if timeline.unit_column else "Action Description",
+        value=f"```{action_string if not timeline.unit_column else flex_string}```",
+        inline=True)
+    embed.add_field(
+        name="Action Description" if timeline.unit_column else "Set Status",
+        value=f"```{flex_string if not timeline.unit_column else action_string}```",
+        inline=True)
+
     await ctx.respond(embed=embed, ephemeral=not show)
+
 
 
 # =============== List all TLs for a boss =============
@@ -224,7 +334,7 @@ async def list_tls(ctx, boss,
                         color=0xfffeff)
     await ctx.respond(embed=embed, ephemeral=True, view=view)
 
-
+'''
 # =============== Deletes one timeline with specified id =============
 @bot.slash_command(guild_ids=guild_id, description="Deletes target TL")
 @commands.has_role(1025780684574433390)
@@ -264,6 +374,7 @@ async def delete_tl(ctx, id):
     await ctx.respond(embed=embed, ephemeral=True)
     await ctx.respond("Are you SURE you want to delete this TL?", view=view, ephemeral=True)
 
+'''
 
 # =============== Reload translations from woody translation sheet =============
 @bot.slash_command(guild_ids=guild_id, description="Pulls the latest woody-grade translations")
@@ -286,7 +397,7 @@ async def help(ctx):
         inline=False)
 
     embed.add_field(
-        name="/transcribe_tl - Transcribes a timeline and stores it for further use",
+        name="/transcribe_tl - Transcribes a timeline and stores it (ADMIN ONLY)",
         value="```\tboss: Boss that you want this TL to target (1-5)" +
             "\n\tunit1-5: The five units that are a part of the TL" +
             "\n\ttl: Attached file containing timeline" +
